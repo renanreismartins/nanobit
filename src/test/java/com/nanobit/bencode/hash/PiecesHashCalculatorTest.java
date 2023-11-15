66964b16b1e3c8440f0f8289e5169c21a20b68f9
Renan Reis Martins de Paula <renanreismartins@gmail.com>
2023-11-15 23:04:16 +0000

    Explanation why we do not need to convert the bytes to unsigned

--- src/test/java/com/nanobit/bencode/hash/PiecesHashCalculatorTest.java
+++ src/test/java/com/nanobit/bencode/hash/PiecesHashCalculatorTest.java
@@ -1,6 +1,5 @@
 package com.nanobit.bencode.hash;
 
-import com.nanobit.bencode.hash.PiecesHashCalculator;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -14,6 +13,39 @@ class PiecesHashCalculatorTest {
 
 	@Test
 	void calculatePieceHashesHex() throws IOException {
+		/*
+		Why there is no need for transforming the bytes to unsigned:
+
+		System.out.println(Arrays.toString(pieces)); [-24, ...]
+		First byte = -24
+
+		Transform the absolute value to decimal -> 24 = 00011000
+
+		Use the 2's complement because the original value is negative,
+		Invert all the bits and then add 1: 11101000
+
+		Convert the 2's complement to decimal: 11101000 -> 232
+
+		Transform the decimal 232 to hex:
+		232 / 16 = 14
+		15 * 16 = 224
+
+		232 - 224 = 8
+
+		16 ^ 0 = 1     - 8  = 8
+		16 ^ 1 = 16    - 14 = e
+		16 ^ 2 = 256   - 0
+
+		0xe8
+
+		String.format("%02x", signedByte) works even with a signed byte because it considers the most significant bit as
+		data, so when converting the byte to hex it does it correctly:
+		-24 = 11101000 (signed byte)
+		11101000 = e8 in hex
+		11101000 = 232 in decimal using unsigned byte
+		232 = e8 in hex
+		 */
+		
 		byte[] pieces = Files.readAllBytes(Paths.get("src/test/resources/pieces.hash"));
 
 		List<String> piecesHashesHex = new PiecesHashCalculator(92063, 32768, pieces).piecesHashesHex();
